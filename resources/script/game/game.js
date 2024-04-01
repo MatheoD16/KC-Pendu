2383a91b0b66ca2d4f9b3772b140178883b376ad
Matheo Dupuis <matheodupuis1@gmail.com>
2024-04-01 19:19:25 +0200

    :bug: Modif lien image

--- resources/script/game/game.js
+++ resources/script/game/game.js
@@ -34,7 +34,7 @@ export class Game {
     this.hiddenLetter = [];
 
     // Set all boolean of hiddenLetter to false
-    for (let letter of this.hiddenWord){
+    for (let letter of this.hiddenWord) {
       this.hiddenLetter.push(false);
     }
 
@@ -46,7 +46,6 @@ export class Game {
     //Add an event to restart button
     const element = document.getElementById("btn-restart");
     element.addEventListener("click", this.reload.bind(this));
-
   }
 
   //Reload
@@ -54,7 +53,7 @@ export class Game {
   /**
    * When reload button is clicked this function start a new game
    */
-  reload(){
+  reload() {
     this.keyBoard.enableKeyboard();
     this.clearWord();
 
@@ -62,7 +61,7 @@ export class Game {
     this.tries = 8;
     this.hiddenLetter = [];
 
-    for (let letter of this.hiddenWord){
+    for (let letter of this.hiddenWord) {
       this.hiddenLetter.push(false);
     }
 
@@ -73,10 +72,10 @@ export class Game {
   /**
    * Clear all characters for the hidden word in the view
    */
-  clearWord(){
+  clearWord() {
     const element = document.getElementById("hidden_word");
     for (let i = 0; i < this.hiddenWord.length; i++) {
-      let child = document.getElementById("char-"+i);
+      let child = document.getElementById("char-" + i);
       element.removeChild(child);
     }
   }
@@ -86,25 +85,24 @@ export class Game {
   /**
    * Display the remaining tries before the end of the game
    */
-  displayTries(){
+  displayTries() {
     const img = document.getElementById("image-bw");
     const element = document.getElementById("game-count");
 
-    if (this.checkWordFound()){
-      img.src = "/resources/images/blue_wall_win.jpg";
-      element.textContent = "Bravo vous avez gagné !"
-    }
-    else {
-      img.src = "/resources/images/blue_wall_" + this.tries + ".jpg";
+    if (this.checkWordFound()) {
+      img.src = "../images/blue_wall_win.jpg";
+      element.textContent = "Bravo vous avez gagné !";
+    } else {
+      img.src = "../images/blue_wall_" + this.tries + ".jpg";
 
       if (this.tries <= 0) {
-        element.textContent = "Vous avez perdu ! La personne à trouver était " + this.hiddenWord
+        element.textContent =
+          "Vous avez perdu ! La personne à trouver était " + this.hiddenWord;
       } else {
         element.textContent = "Il vous reste " + this.tries + " tentatives";
       }
     }
   }
-
 
   //KeyBoard/Button
 
@@ -129,10 +127,10 @@ export class Game {
    * Check if all the characters of the hidden word are found
    * @returns {*|boolean}
    */
-  checkWordFound(){
-    for (let find of this.hiddenLetter){
-      if (! find){
-        return find
+  checkWordFound() {
+    for (let find of this.hiddenLetter) {
+      if (!find) {
+        return find;
       }
     }
     return true;
@@ -142,15 +140,15 @@ export class Game {
    * Check if the current button clicked has already been used
    * @param button
    */
-  checkClickedButton(button){
-    if ((this.tries > 0) && (! this.checkWordFound()) && (! button.used)) {
-      const check = this.isInWord(this.hiddenWord, button)
-      button.inWord = check[0]
+  checkClickedButton(button) {
+    if (this.tries > 0 && !this.checkWordFound() && !button.used) {
+      const check = this.isInWord(this.hiddenWord, button);
+      button.inWord = check[0];
       if (button.inWord) {
         button.disableGreen();
         this.displayChar(button);
       } else {
-        this.tries --;
+        this.tries--;
         button.disableRed();
       }
     }
@@ -167,7 +165,6 @@ export class Game {
     }
   }
 
-
   //HiddenWord
 
   /**
@@ -176,11 +173,11 @@ export class Game {
    * @param button
    * @returns {*[]} An array of index
    */
-  getCharsIndex(button){
+  getCharsIndex(button) {
     const letter = document.getElementById(button.id).textContent.trim();
     let chars = [];
-    for (let i = 0 ; i < this.hiddenWord.length; i++){
-      if (this.hiddenWord[i] === letter){
+    for (let i = 0; i < this.hiddenWord.length; i++) {
+      if (this.hiddenWord[i] === letter) {
         chars.push(i);
       }
     }
@@ -191,10 +188,10 @@ export class Game {
    * Display the character found in the view
    * @param button
    */
-  displayChar(button){
+  displayChar(button) {
     const index = this.getCharsIndex(button);
-    for (let i of index){
-      let field = document.getElementById("char-"+i);
+    for (let i of index) {
+      let field = document.getElementById("char-" + i);
       field.textContent = this.hiddenWord[i];
       this.hiddenLetter[i] = true;
     }
@@ -207,7 +204,7 @@ export class Game {
    */
   RandomHiddenWord() {
     const key = Math.floor(Math.random() * Object.keys(data).length);
-    const word = String(data[key])
+    const word = String(data[key]);
     return word.toUpperCase();
   }
 
@@ -232,11 +229,8 @@ export class Game {
   generateCell(letter, index) {
     const cell = document.createElement("p");
     cell.textContent = "-";
-    cell.id = "char-"+index;
+    cell.id = "char-" + index;
     cell.setAttribute("class", "p-1");
     return cell;
   }
-
-
-
 }
