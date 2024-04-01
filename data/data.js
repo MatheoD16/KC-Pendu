1cde2386166317474b9e8fcb3007ab45c4ce8a0a
Matheo Dupuis <matheodupuis1@gmail.com>
2024-04-01 18:19:45 +0200

    :card_file_box: Update database

--- data/data.js
+++ data/data.js
@@ -5,25 +5,128 @@ export const keyBoardBtn = [
 ]
 
 export const data = {
-  0: "Kameto",
-  1: "Cabochard",
-  2: "Bo",
-  3: "Saken",
-  4: "Upset",
-  5: "Targamas",
-  6: "Maynter",
-  7: "Lyncas",
-  8: "Vladi",
-  9: "Caliste",
-  10: "Fleshy",
-  11: "Shin",
-  12: "Magnum",
-  13: "Tomaszy",
-  14: "Marteen",
-  15: "N4rrate",
-  16: "Vatira",
-  17: "Rise",
-  18: "Atow",
-  19: "Canbizz",
-  20: "Double",
+  //LoL
+  //LEC
+  0: "Cabochard",
+  1: "Bo",
+  2: "Saken",
+  3: "Upset",
+  4: "Targamas",
+
+  //Coach LEC
+  5: "Reha",
+  6: "Steak",
+  7: "Shaves",
+
+  //LFL
+  8: "Maynter",
+  9: "Lyncas",
+  10: "Vladi",
+  11: "Caliste",
+  12: "Fleshy",
+
+  //Coach LFL
+  13: "Wadi",
+  14: "Apples",
+  15: "Souf",
+
+  //Ancien LoL
+  16: "Wao",
+  17: "Skeanz",
+  18: "Striker",
+  19: "whiteinn",
+  20: "Hantera",
+  21: "Rekkles",
+  22: "113",
+  23: "Kaori",
+  24: "Cinkrof",
+  25: "xMatty",
+  26: "Adam",
+  27: "Helaz",
+  28: "jujutw0",
+  29: "Nuclearint",
+  30: "Nuc",
+  31: "Rejakar",
+
+  //TFT
+  32: "Double61",
+  33: "CanbizZ",
+
+  //Trackmania
+  34: "Bren",
+  35: "DarkeR",
+  36: "Otaaaq",
+
+  //RL
+  37: "Atow",
+  38: "Vatira",
+  39: "rise",
+  //Coach RL
+  40: "Ferra",
+  41: "Jordi",
+
+  //Ancien RL
+  42: "Eversax",
+  43: "ExoTiiK",
+  44: "itachi",
+  45: "Noly",
+  46: "AztraL",
+  47: "Stake",
+
+  //Valorant VCT
+  48: "Magnum",
+  49: "Shin",
+  50: "marteen",
+  51: "N4RRATE",
+  52: "tomaszy",
+
+  //VCT Coach
+  53: "Engh",
+  54: "ZE1SH",
+  55: "Youenn",
+
+  //Game Changer
+  56: "Nelo",
+  57: "Matriix",
+  58: "Glance",
+  59: "Kuripuuya",
+  60: "Speedowka",
+
+  //Coach Game Changer
+  61: "LaAw",
+  62: "uNKOE",
+  63: "Fabkill",
+
+  //Ancien VCT
+  64: "Nivera",
+  65: "xms",
+  66: "Scream",
+  67: "TakaS",
+  68: "Newzera",
+  69: "Mikee",
+  70: "Amilwa",
+
+  //Ancien GC
+  71: "Amilwa",
+  72: "LiriLia",
+  73: "Mel",
+  74: "Ninou",
+
+  //Smash
+  75: "Kurama",
+
+  //Autre
+  76: "White",
+  77: "Jiizuke",
+  78: "Zoelys",
+  79: "Denvoksne",
+  80: "Nalkya",
+  81: "Nerroh",
+  82: "Yamato",
+
+  //CEO
+  83: "Kameto",
+  84: "Prime",
+  85: "Arthur",
+  86: "Kotei"
 };
